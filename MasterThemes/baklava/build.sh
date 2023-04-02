35772a2a12255fe1bd61dae35976195a8f0566c8
amgeekz <79773348+amgeekz@users.noreply.github.com>
2023-04-03 05:39:52 +0700

    Update build.sh

--- MasterThemes/baklava/build.sh
+++ MasterThemes/baklava/build.sh
@@ -21,7 +21,7 @@ if [ ! -f "artisan" ]; then
     zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources 2> /dev/null
 
     echo "Downloading the Theme you picked"
-    mkdir -p tempdown && cd tempdown && git clone https://github.com/Humpol/pt-themes .
+    mkdir -p tempdown && cd tempdown && git clone https://github.com/amgeekz/epromite .
     cp -r MasterThemes/baklava/config ..
     cp -r MasterThemes/baklava/public ..
     cp -r MasterThemes/baklava/resources ..
